af1a1f741c6484c20e2f1062aff386042737552c
unknown <karenwli@seas.upenn.edu>
2022-01-26 23:24:05 -0800

    'first

---
+++ sign.py
@@ -0,0 +1,18 @@
+from fastecdsa.curve import secp256k1
+from fastecdsa.keys import export_key, gen_keypair
+
+from fastecdsa import curve, ecdsa, keys, point
+from hashlib import sha256
+
+
+def sign(m):
+    # generate public key
+    # Your code here
+    # public_key = None
+    private_key, public_key = keys.gen_keypair(curve.secp256k1)
+    # generate signature
+    r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
+    assert isinstance(public_key, point.Point)
+    assert isinstance(r, int)
+    assert isinstance(s, int)
+    return (public_key, [r, s])
